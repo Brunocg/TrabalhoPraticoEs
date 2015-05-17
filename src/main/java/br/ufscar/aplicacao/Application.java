6532959e53515ec448a9c13f6d304b9ba36c85fe
Bruno <bruno_cesar@outlook.com>
2015-05-17 13:50:30 -0300

    Configurando ambiente.

--- src/main/java/br/ufscar/aplicacao/Application.java
+++ src/main/java/br/ufscar/aplicacao/Application.java
@@ -1,10 +1,8 @@
 package br.ufscar.aplicacao;
 
-import java.util.Date;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
+//import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "br.ufscar")
-@EntityScan(basePackages = "br.ufscar")
+//@EntityScan(basePackages = "br.ufscar")
 public class Application {
 
 	public static void main(String[] args) {
