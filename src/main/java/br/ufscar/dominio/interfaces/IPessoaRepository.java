5371b07b6d42f516362f06eb7729ca545fd5bed0
Andre Landi <a_souzalandy@yahoo.com.br>
2015-06-12 11:04:10 -0300

    Atualizacao da Interface Pessoa Repository

--- src/main/java/br/ufscar/dominio/interfaces/IPessoaRepository.java
+++ src/main/java/br/ufscar/dominio/interfaces/IPessoaRepository.java
@@ -1,17 +1,31 @@
 package br.ufscar.dominio.interfaces;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import br.ufscar.dominio.CompetenciaExperiencia;
+import br.ufscar.dominio.Endereco;
 import br.ufscar.dominio.Pessoa;
+import br.ufscar.dominio.Usuario;
 
 public interface IPessoaRepository {
 
-	int novoId();
-	Pessoa buscarPorId(int pessoaId);
-	Pessoa buscarPorLogin(String login);
-	void inserir(Pessoa pessoa);	
-	void editar(Pessoa pessoa);
-	boolean excluir(Pessoa pessoa);
-	Page<Pessoa> listar(Pageable pageable);
+	public int novoId();
+	public Pessoa buscarPorId(int pessoaId);
+	public Pessoa buscarPorLogin(String login);
+	public void editar(Pessoa pessoa);
+	public boolean excluir(Pessoa pessoa);
+	public Page<Pessoa> listar(Pageable pageable);
+	
+	public boolean gravaPessoa(Pessoa pessoa);
+	public boolean gravaUsuario(Pessoa pessoa, Usuario usuario) throws SQLException;
+	public boolean gravaExperiencias(Pessoa pessoa,	List<CompetenciaExperiencia> competenciasExperiencia) throws SQLException;
+	public boolean gravaExperiencias(Pessoa pessoa, CompetenciaExperiencia competenciaExperiencia) throws SQLException;
+	public boolean gravaEmderecosPessoa(Pessoa pessoa, List<Endereco> enderecos) throws SQLException;
+	public boolean gravaEmderecosPessoa(Pessoa pessoa, Endereco endereco) throws SQLException;
+	public List<Endereco> gravaEndereco(List<Endereco> enderecos) throws SQLException;
+	public int gravaEndereco(Endereco endereco) throws SQLException;
 }
