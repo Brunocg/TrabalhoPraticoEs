083ce0911d029137170e2a9ed2535eb50a4cf3df
Erick Previato <erick.previato@gmail.com>
2015-07-12 13:06:30 -0300

    Inserindo as telas de listagem e cadastro de todos os itens de menu. Habilitando inserção, edição, remoção e listagem dos gerentes. Habilitando inserção e listagem dos projetos.

--- src/main/resources/public/resources/js/appController.js
+++ src/main/resources/public/resources/js/appController.js
@@ -1,15 +1,3 @@
-// Funcao para formatar dados do form html
-function getFormData($form){
-    var unindexed_array = $form.serializeArray();
-    var indexed_array = {};
-
-    $.map(unindexed_array, function(n, i){
-        indexed_array[n['name']] = n['value'];
-    });
-
-    return indexed_array;
-}
-
 function obterPessoa($id) {
 	$.ajax({
 	    type: "get",
@@ -28,15 +16,8 @@ function obterPessoa($id) {
 	});
 }
 
-function showListGerente() {
-	$("#page-wrapper").load("resources/view/gerente/listar.html");
-	$(".navbar-header button").click();
-	$('html, body').animate({ scrollTop: 0 }, 200);
-}
-
 function editarPessoa($id) {
 	$("#page-wrapper").load("resources/view/gerente/cadastro.html");
-	$(".navbar-header button").click();
 	$('html, body').animate({ scrollTop: 0 }, 200);
 
 	$.ajax({
@@ -53,7 +34,7 @@ function editarPessoa($id) {
 	    		$("#email").val(data.data.email);
 	    		$("#pagPessoal").val(data.data.pagPessoal);
 	    		$("#msgInst").val(data.data.msgInst);
-	    		$("#login").val(data.data.login);
+	    		$("#login").val(data.data.usuario.login);
 	    		$("#idPessoa").val($id);
 	    		
 	    		$(".subtitulo").html("Editar usuário");
@@ -66,6 +47,8 @@ function editarPessoa($id) {
     		showMessage($msg, $type);
 	    }
 	});
+	
+	iniciarForms();
 }
 
 function excluirPessoa($id) {
@@ -100,37 +83,155 @@ function showMessage($msg, $type) {
 
     Messenger().post({
         message: $msg,
-        id: "Only-one-message",
+        id: "Only-one-message",	
         type: $type,
         showCloseButton: true
     });
 }
 
-function listarGerentes() {
+function iniciarBotoes(){
+	$(".btn-novo").on("click", function(){
+		var $tipo = $(this).attr('tipo');
+		$.get("resources/view/"+$tipo+"/cadastro.html", function(data){
+			$("#page-wrapper").html(data);
+			iniciarForms();
+	    });
+		$('html, body').animate({ scrollTop: 0 }, 200);
+	});
+	$(".btn-todos").on("click", function(){
+		var $tipo = $(this).attr('tipo');
+		$.get("resources/view/"+$tipo+"/listar.html", function(data){
+			$("#page-wrapper").html(data);
+			iniciarForms();			
+	    });
+		$('html, body').animate({ scrollTop: 0 }, 200);
+		if ($tipo != 'projeto') {
+			listar('pessoa');
+		} else {
+			listar($tipo);
+		}	
+	});
+	$(".btn-perfil").on("click", function(){
+		$("#page-wrapper").load("resources/view/pessoa/perfil.html");
+		$('html, body').animate({ scrollTop: 0 }, 200);
+	})
+}
+
+function iniciarForms() {
+	// Funcao para formatar dados do form html
+	function getFormData($form){
+	    var unindexed_array = $form.serializeArray();
+	    var indexed_array = {};
+
+	    $.map(unindexed_array, function(n, i){
+	        indexed_array[n['name']] = n['value'];
+	    });
+
+	    return indexed_array;
+	}
+
+	// Inserir um novo gerente
+	$("#gerenteForm").on("submit", function(){
+		$url = ($(".btn-cadastrar-usuario").val() == "Editar") ? "pessoa/editar" : "pessoa/inserir/basico";
+		
+		var $pessoa = getFormData($(this));
+		$usuario = {login:$("#login").val(), senha:$("#senha").val(), usuarioTipo:"Colaborador"};
+		$pessoa.usuario = $usuario;
+		
+		$.ajax({
+		    type: "POST",
+		    url: $url,
+		    data: JSON.stringify($pessoa),
+		    contentType: "application/json; charset=utf-8",
+		    dataType: "json",
+		    success: function(data){
+		    	if(data.success == false) {
+		    		$msg = 'Erro ao salvar os dados! Tente novamente mais tarde!';
+		    		$type = 'error';
+		    		showMessage($msg, $type);
+		    	} else {
+		    		$msg = 'Dados salvos com sucesso!';
+		    		$type = 'success';
+		    		showMessage($msg, $type);
+		    		$("#page-wrapper").load("resources/view/gerente/listar.html");
+		    		listar('pessoa');
+		    		$('html, body').animate({ scrollTop: 0 }, 200);
+		    	}
+	    	},
+		    failure: function(errMsg) {
+		        alert(errMsg);
+		    }
+		});
+		return false;
+	});
+
+	$("#projetoForm").on("submit", function(){
+		$.ajax({
+		    type: "POST",
+		    url: '/projeto/inserir',
+		    data: JSON.stringify(getFormData($(this))),
+		    contentType: "application/json; charset=utf-8",
+		    dataType: "json",
+		    success: function(data){
+		    	if(data.success == false) {
+		    		$msg = 'Erro ao salvar os dados! Tente novamente mais tarde!';
+		    		$type = 'error';
+		    		showMessage($msg, $type);
+		    	} else {
+		    		$msg = 'Dados salvos com sucesso!';
+		    		$type = 'success';
+		    		showMessage($msg, $type);
+		    		$("#page-wrapper").load("resources/view/projeto/listar.html");
+		    		listar('gerente');
+		    		$('html, body').animate({ scrollTop: 0 }, 200);
+		    	}
+	    	},
+		    failure: function(errMsg) {
+		        alert(errMsg);
+		    }
+		});
+		return false;
+	});
+}
+
+function listar($tipo) {
 	$.ajax({
 	    type: "GET",
-	    url: "/pessoa/listar",
+	    url: "/"+$tipo+"/listar",
 	    success: function(data){
 	    	if(data.data == null) {
 	    		$msg = 'Erro ao listar usuários!';
 	    		$type = 'error';
 	    		showMessage($msg, $type);
 	    	} else {
-	    		$("#tableGerente tbody").html("");
+	    		$("#table"+$tipo+" tbody").html("");
 	    		for (var $i = 0; $i < data.data.length; $i++) {
-	    			var $texto = '<tr role="row">';
-	    			$texto += "<td>"+data.data[$i].nome+"</td>";
-	    			$texto += "<td>"+data.data[$i].cpf+"</td>";
-	    			$texto += "<td>"+data.data[$i].sitCivil+"</td>";
-	    			$texto += "<td>"+data.data[$i].sexo+"</td>";
-	    			$texto += "<td>"+data.data[$i].dataNascimento+"</td>";
-	    			$texto += "<td>"+data.data[$i].estado+"</td>";
-	    			$texto += "<td>"+data.data[$i].login+"</td>";
-	    			var $botao1 = '<button class="btn btn-green btn-xs btn-editar" value="'+data.data[$i].idPessoa+'" title="Editar"><i class="fa fa-pencil-square-o"></i></button> &nbsp; ';
-			        var $botao2 = '<button class="btn btn-red btn-xs btn-excluir" value="'+data.data[$i].idPessoa+'" title="Excluir"><i class="fa fa-times"></i></button>';
-			        $texto += "<td>"+$botao1+$botao2+"</td>";
-			        $texto += '</tr>';
-			        $("#tableGerente tbody").append($texto);
+	    			if ($tipo == 'pessoa') {
+		    			var $texto = '<tr role="row">';
+		    			$texto += "<td>"+data.data[$i].nome+"</td>";
+		    			$texto += "<td>"+data.data[$i].cpf+"</td>";
+		    			$texto += "<td>"+data.data[$i].sitCivil+"</td>";
+		    			$texto += "<td>"+data.data[$i].sexo+"</td>";
+		    			$texto += "<td>"+data.data[$i].dataNascimento+"</td>";
+		    			$texto += "<td>"+data.data[$i].estado+"</td>";
+		    			$texto += "<td>"+data.data[$i].usuario.login+"</td>";
+		    			var $botao1 = '<button class="btn btn-green btn-xs btn-editar" value="'+data.data[$i].idPessoa+'" title="Editar"><i class="fa fa-pencil-square-o"></i></button> &nbsp; ';
+				        var $botao2 = '<button class="btn btn-red btn-xs btn-excluir" value="'+data.data[$i].idPessoa+'" title="Excluir"><i class="fa fa-times"></i></button>';
+				        $texto += "<td>"+$botao1+$botao2+"</td>";
+				        $texto += '</tr>';
+	    			} else if ($tipo == 'projeto') {
+	    				var $texto = '<tr role="row">';
+		    			$texto += "<td>"+data.data[$i].nome+"</td>";
+		    			$texto += "<td>"+data.data[$i].tipo+"</td>";
+		    			$texto += "<td>"+data.data[$i].prazo+"</td>";
+		    			$texto += "<td>"+data.data[$i].observacoes+"</td>";
+		    			$texto += "<td>"+data.data[$i].status+"</td>";
+		    			var $botao1 = '<button class="btn btn-green btn-xs btn-editar" value="'+data.data[$i].idPessoa+'" title="Editar"><i class="fa fa-pencil-square-o"></i></button> &nbsp; ';
+				        var $botao2 = '<button class="btn btn-red btn-xs btn-excluir" value="'+data.data[$i].idPessoa+'" title="Excluir"><i class="fa fa-times"></i></button>';
+				        $texto += "<td>"+$botao1+$botao2+"</td>";
+				        $texto += '</tr>';
+	    			}
+			        $("#table"+$tipo+" tbody").append($texto);
 	    		}
 	    		$(".listagem-gerentes button").tooltip();
 	    		$(".btn-editar").on("click", function(){
@@ -139,7 +240,7 @@ function listarGerentes() {
 	    		$(".btn-excluir").on("click", function(){
 	    			excluirPessoa($(this).val());
 	    		});
-	    		$("#tableGerente").dataTable();
+	    		$("#table"+$tipo).dataTable();
 	    	}
     	},
 	    failure: function(errMsg) {
@@ -170,73 +271,27 @@ $(document).ready(function() {
 	// Funcoes de menu
 	$(".dropdown-toggle").dropdown();
 	
-	// Funcoes da parte de gerente
-	$(".btn-gerente-novo").on("click", function(){
-		$("#page-wrapper").load("resources/view/gerente/cadastro.html");
-		$(".navbar-header button").click();
-		$('html, body').animate({ scrollTop: 0 }, 200);
-	});
+	iniciarBotoes();
 	
-	$(".btn-gerente-todos").on("click", function(){
-		showListGerente();
-	});
+	iniciarForms();
 	
-	$(".btn-cadastrar-usuario").on("click", function(){
-		
-		cadastrarPessoa($url);
-	});
-	
-	// Inserir um novo gerente
-	$("#gerenteForm").on("submit", function(){
-		$url = ($(".btn-cadastrar-usuario").val() == "Editar") ? "pessoa/editar" : "pessoa/inserir/basico";
-		
-		var $pessoa = getFormData($(this));
-		$usuario = {login:$("#login").val(), senha:$("#senha").val()};
-		$pessoa.usuario = $usuario;
-		
+	// Metodo para encerrar sessao e sair 
+	$(".btn-logout").on("click", function(){
 		$.ajax({
-		    type: "POST",
-		    url: $url,
-		    data: JSON.stringify($pessoa),
-		    contentType: "application/json; charset=utf-8",
-		    dataType: "json",
+		    type: "GET",
+		    url: "/pessoa/usuario/logout",
 		    success: function(data){
-		    	if(data.success == false) {
-		    		$msg = 'Erro ao salvar os dados! Tente novamente mais tarde!';
+		    	if(data.data == null) {
+		    		window.location.href="/";
+		    	} else {
+		    		$msg = 'Erro ao fazer logout!';
 		    		$type = 'error';
 		    		showMessage($msg, $type);
-		    	} else {
-		    		$msg = 'Dados salvos com sucesso!';
-		    		$type = 'success';
-		    		showMessage($msg, $type);
-		    		showListGerente();
 		    	}
 	    	},
 		    failure: function(errMsg) {
 		        alert(errMsg);
 		    }
 		});
-		return false;
 	});
-	
-	
-	// Metodo para encerrar sessao e sair 
-	$(".btn-logout").on("click", function(){
-		$.ajax({
-	    type: "GET",
-	    url: "/pessoa/usuario/logout",
-	    success: function(data){
-	    	if(data.data == null) {
-	    		window.location.href="/";
-	    	} else {
-	    		$msg = 'Erro ao fazer logout!';
-	    		$type = 'error';
-	    		showMessage($msg, $type);
-	    	}
-    	},
-	    failure: function(errMsg) {
-	        alert(errMsg);
-	    }
-	});
-	})
 });
